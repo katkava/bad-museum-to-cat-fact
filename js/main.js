9473a772adc535bbc261e88702166fcb6502e39b
KatKava <katryn.kav97@gmail,comgit config --global user.email katryn.kav97@gmail.com>
2021-02-04 19:19:10 +0000

    changed JS  and fixed bug to get desired output

--- js/main.js
+++ js/main.js
@@ -6,23 +6,56 @@ function getFetch(){
   const artist = document.querySelector('input').value
   const url = "https://collectionapi.metmuseum.org/public/collection/v1/search?q="+artist
   const secondUrl = "https://cat-fact.herokuapp.com/facts"
-  let numberIWant = []
-  let someCatFact = []
+  // let numberIWant = []
+  // let someCatFact = []
+
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        numberIWant.push(data.total)
+    .then(res => res.json())
+    .then(museumData => {
+  fetch(secondUrl).then(res => res.json()).then(catData => {
+    const numberIWant = museumData.total;
+    const someCatFact = catData[0].text;
+
+    document.querySelector('h2').innerText = someCatFact.repeat(numberIWant)
+  });
+  });
+}
+//   .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });   
+//  };
+//  }
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });    
+ 
+// }
+
+  // fetch(url)
+  //     .then(res => res.json()) // parse response as JSON
+  //     .then(data => {
+  //       numberIWant.push(data.total)
       
 
        
-  fetch(secondUrl)
-      .then(res => res.json()) // parse response as JSON
-      .then( function (data){
+  // fetch(secondUrl)
+  //     .then(res => res.json()) // parse response as JSON
+  //     .then( function (data){
         
-        // num = Math.floor(Math.random()*numberOfArtists);
-         someCatFact.push(data[0].text)
+  //       // num = Math.floor(Math.random()*numberOfArtists);
+  //        someCatFact.push(data[0].text)
 
-         console.log(someCatFact.repeat(numberIWant))
+  //        console.log(someCatFact.repeat(numberIWant))
 
         //  console.log(factNum)
 
@@ -33,20 +66,18 @@ function getFetch(){
 
          
       
-      //  document.querySelector('h2').innerText = numberOfObject
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+//       //  document.querySelector('h2').innerText = numberOfObject
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
 
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-      
-      //  document.querySelector('h2').innerText = numberOfObject      
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });    
  
-}
+// }
 
 
 
